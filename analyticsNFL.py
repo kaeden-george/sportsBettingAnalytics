76795a22834d38bdc14609dcffe8ef4a72e0bfff
Kaeden George <kaeden.george01@gmail.com>
2025-09-29 23:38:55 -0600

    Added Data Scraping

--- analyticsNFL.py
+++ analyticsNFL.py
@@ -2,8 +2,29 @@ import numpy as np
 import pandas as pd
 import requests
 
+#import the data scraper
+from nfl_scraper import NFLDataScraper
 
-#Access some sort of NFL API
 
+#Create an instance of the NFLDataScraper class
+nfl_scraper = NFLDataScraper()
 
-#Monte Carlo of some sort????
+#Parameters
+
+#TODO: Check if I can actually pull 2025 stats
+nfl_scraper.season = "2025"
+
+#Get current season data
+for level in ["player", "team"]:
+    nfl_scraper.get_stats(level)
+
+#get historic data
+
+# Create a liPst of years as strings since 1970
+seasons = [str(year) for year in range(1970, nfl_scraper.current_season - 1)]
+
+# Loop through seasons and levels to retrieve historical data
+for season in seasons:
+    for level in ["player", "team"]:
+        nfl_scraper.season = season  # Set the season for scraping
+        nfl_scraper.get_stats(level)
